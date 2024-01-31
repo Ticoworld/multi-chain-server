cdc6911094040c93c8b7109d2fe19045f8622c16
Ticoworld <timothyticoworld@gmail.com>
2024-01-31 11:18:31 +0100

    bcryptjs add

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors  = require('cors')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs')
 const saltRounds = 10; 
 app.use(cors())
 app.use(express.json())
